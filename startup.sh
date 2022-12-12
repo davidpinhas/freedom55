0ee66eca713ac052e59312028579a96d2684fcbc
David Pinhas <davepinhas89@gmail.com>
2022-12-12 21:12:01 +0200

    fix startup.sh script and add init steps for local env

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-if [ -d "/venv" ]; then
+if [ -d "venv" ]; then
   # Virtualenv Exists
   source venv/bin/activate
   pip install -r requirements.txt
@@ -12,3 +12,14 @@ else
   pip install -r requirements.txt
   python3.10 setup.py develop
 fi
+
+current_dir=$PWD
+alias_name=$(printf "alias argusCLI='cd %s/argus && source venv/bin/activate'" "$current_dir")
+
+echo "Argus was initialized successfully.
+To configure the CLI on the local machine, Run the following commands:
+"
+printf 'echo "%s" >> %s' "$alias_name" "~/.bashrc"
+echo "
+source ~/.bashrc
+argusCLI"
