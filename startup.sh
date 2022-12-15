9a10f77b36a2c13875a2e0daba1e56d23c410440
David Pinhas <davepinhas89@gmail.com>
2022-12-16 01:29:00 +0200

    remove cd command in startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,6 @@ if [ -d "venv" ]; then
   source venv/bin/activate
   pip install -r requirements.txt
   python3.10 setup.py develop
-  cd ../
 else
   # Virtualenv created
   virtualenv --python=python3.10 venv
