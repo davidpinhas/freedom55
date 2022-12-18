ad795b91c33721a5c98178387af00ea885fbd193
David Pinhas <davepinhas89@gmail.com>
2022-12-18 23:39:28 +0200

    reformat setup.py file

--- setup.py
+++ setup.py
@@ -1,35 +1,29 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
+with open("README.md", "r") as f:
+    readme = f.read()
 
 setup(
-    name='argus',
-    version='1.0.0',
-    description='Argus CLI is a operational tool to maintain a personal homelab',
-    long_description=README,
+    name="argus",
+    version="1.0.0",
+    description="Argus CLI is a operational tool to maintain a personal homelab",
+    long_description=readme,
     long_description_content_type="text/markdown",
-    license='MIT',
-
-    author='Dave Pinhas',
-    author_email='davepinhas89@gmail.com',
-    url='ssh://git@github.com:davidpinhas/argus.git',
-
-    classifiers=[
-        'Programming Language :: Python :: 3.10'
+    license="MIT",
+    author="Dave Pinhas",
+    author_email="davepinhas89@gmail.com",
+    url="ssh://git@github.com:davidpinhas/argus.git",
+    classifiers=["Programming Language :: Python :: 3.10"],
+    packages=[
+        "cli/cli_groups",
+        "cli/oci_client",
+        "cli/argocd_client",
+        "cli/sops_client",
+        "cli/tf_client",
+        "cli",
+        "utils",
     ],
-
-    packages=find_packages(),
     include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'argus = cli.argus:main'
-        ]
-    },
-
-    install_requires=[
-        'click',
-        'oci',
-        'requests',
-        'python_terraform'
-    ],
+    entry_points={"console_scripts": ["argus = cli.argus:main"]},
+    install_requires=["click", "oci", "requests", "python_terraform"],
 )
