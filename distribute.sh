1a7ceec5c9f139c2c77f92e55b7ef39495331d15
David Pinhas <davepinhas89@gmail.com>
2022-12-19 00:08:04 +0200

    create distribution script with twine

---
+++ distribute.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Validate arguments
+if [ "$#" -ne 3 ]; then
+  echo "Error: script requires three arguments (username, password and repository-url)"
+  echo "Usage: script.sh [username] [password] [repository-url]"
+  echo "Example: script.sh admin Password1 https://demo.jfrog.io/artifactory/api/pypi/pypi"
+  exit 1
+fi
+
+# Cleanup
+rm -rf arg.spec argus.spec venv build dist ..spec argus.egg-info
+
+# Binary creation
+python3 setup.py sdist bdist_wheel
+
+# User arguments
+username=$1
+password=$2
+repository_url=$3
+
+# Run the twine upload 
+twine upload --username $username --password $password --repository-url $repository_url dist/*
