c3a063a8f5099e162b7e93d12efa5ff4accdd0eb
davepinhas89@gmail.com <davepinhas89@gmail.com>
2022-12-24 20:04:22 +0000

    Update version to 2.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="fd55",
-    version="2.2.1",
+    version="2.2.2",
     description="Freedom 55 CLI is a operational tool to maintain a personal homelab",
     long_description=readme,
     long_description_content_type="text/markdown",
