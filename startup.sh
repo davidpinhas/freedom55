d5fb468d0d4b2af327b808828e93258e65751000
David Pinhas <davepinhas89@gmail.com>
2022-12-30 00:23:38 +0200

    remove out of scope python installation from startup.sh script

--- startup.sh
+++ startup.sh
@@ -13,49 +13,21 @@ You can install Python 3 using the following commands:
   * Debian/Ubuntu: sudo apt-get install python3 python3-pip python3-virtualenv
   * CentOS/RedHat: sudo yum install python3 python3-pip python3-virtualenv
   * Fedora: sudo dnf install python3 python3-pip python3-virtualenv
-  * macOS: brew install python3 python3-pip python3-virtualenv
+  * macOS: brew install python3 virtualenv
   * Windows:
 
 Download the Python 3 installer from the official Python website: https://www.python.org/downloads/windows/
 Run the installer and follow the prompts to install Python 3.
 "
-  exit 0
+  return 0
+# Check Python 3 version
 else
   minor_version=$(python3 -c 'import sys; print(sys.version_info)' | awk '{print $2}' | grep -o '[0-9]\+')
   if [[ $minor_version -ge 10 ]]; then
     echo "INFO: Valid Python version"
-  # Check the operating system
   else
-    # Install Python 3.10
-    # On Debian/Ubuntu systems
-    if [ -x "$(command -v apt-get)" ]; then
-      sudo apt-get update
-      sudo apt-get install python3.10 python3-virtualenv python3-pip -y
-    # On CentOS/RedHat systems
-    elif [ -x "$(command -v yum)" ]; then
-      sudo yum install python3.10 python3-virtualenv python3-pip -y
-    # On Fedora systems
-    elif [ -x "$(command -v dnf)" ]; then
-      sudo dnf install python3.10 python3-virtualenv python3-pip -y
-    # On macOS systems
-    elif [ "$(uname)" == "Darwin" ]; then
-      # Install Homebrew if it is not already installed
-      if ! [ -x "$(command -v brew)" ]; then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-      fi
-      brew install python3.10 -y
-    # On Windows systems
-    elif [ "$(uname)" == "Windows" ]; then
-      # Download the Python 3.10 installer
-      curl -O https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
-      # Run the installer
-      .\python-3.10.0-amd64.exe
-      # Clean up
-      rm python-3.10.0-amd64.exe
-    else
-      echo "ERROR: Could not detect operating system. Please install Python 3.10 and virtualenv manually."
-      exit 1
-    fi
+    echo "ERROR: Python3 version is lower than 3.10, please install Python3.10 or above before proceeding."
+    echo "INFO: For more details go to - https://www.python.org/downloads."    
   fi
 fi
 # Check if pip3 is installed
@@ -65,7 +37,7 @@ if ! command -v pip3 &> /dev/null; then
   * Debian/Ubuntu: sudo apt-get install python3-pip 
   * CentOS/RedHat: sudo yum install python3-pip
   * Fedora: sudo dnf install python3-pip
-  * macOS: brew install python3-pip
+  * macOS: brew install python3
   * Windows:
 
 Download the Python 3 installer from the official Python website: https://www.python.org/downloads/windows/
@@ -75,9 +47,8 @@ Run the installer and follow the prompts to install Python 3.
 fi
 # Check if virtualenv is installed
 if ! command -v virtualenv &> /dev/null; then
-  # Install virtualenv
-  echo "INFO: Installing Virtualenv"
-  pip3 install python3-virtualenv &> /dev/null
+  echo "Error: Virtualenv not installed"
+  return 0
 fi
 
 # Setting up source code
@@ -102,7 +73,7 @@ else
   python3 setup.py develop &> /dev/null
 fi
 
-# Print the ASCII art and a message to the console
+# Output message
 echo "
 ################
 ##### Done #####
