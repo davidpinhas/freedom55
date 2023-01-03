c0907b93cbbce80f39e3d65ac987625dc24e1927
davepinhas89@gmail.com <davepinhas89@gmail.com>
2023-01-03 23:20:52 +0000

    Update version to 2.5.7

--- cli/fd55.py
+++ cli/fd55.py
@@ -10,7 +10,7 @@ from cli.cli_groups.cli_config import config
 @click.option('-v', '--verbosity', default='info',
               help='Logging level (info, warn, error, debug)',
               required=False)
-@click.version_option(version='2.5.6')
+@click.version_option(version='2.5.7')
 @click.pass_context
 def main(ctx, verbosity):
     import logging
