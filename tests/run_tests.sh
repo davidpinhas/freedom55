2cb11bf60c56d627b981d062a7e3e7a0d781066a
David Pinhas <davepinhas89@gmail.com>
2023-01-13 23:43:19 +0200

    prepare tests script to accept arguments

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,6 +5,115 @@ echo "
 #############################"
 TEST_RESULTS=true
 
+# Check Age exists
+if command -v age >/dev/null 2>&1 ; then
+  age-keygen -o key.txt >/dev/null 2>&1
+else
+  echo "WARN: Age command not found, install Age https://github.com/FiloSottile/age#installation"
+  TEST_RESULTS=false
+fi
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+# Arguments
+case $key in
+    -u|--oci-user)
+    OCI_USER="$2"
+    shift
+    ;;
+    -f|--oci-fingerprint)
+    OCI_FINGERPRINT="$2"
+    shift
+    ;;
+    -t|--oci-tenancy)
+    OCI_TENANCY="$2"
+    shift
+    ;;
+    -r|--oci-region)
+    OCI_REGION="$2"
+    shift
+    ;;
+    -k|--oci-key-file)
+    OCI_KEY_FILE="$2"
+    shift
+    ;;
+    -v|--oci-vault)
+    OCI_VAULT="$2"
+    shift
+    ;;
+    -a|--argocd-url)
+    ARGO_URL="$2"
+    shift
+    ;;
+    -b|--argocd-token)
+    ARGO_TOKEN="$2"
+    shift
+    ;;
+    -e|--cloudflare-email)
+    CLOUDFLARE_EMAIL="$2"
+    shift
+    ;;
+    -c|--cloudflare-api-key)
+    CLOUDFLARE_API_KEY="$2"
+    shift
+    ;;
+    -d|--cloudflare-domain)
+    CLOUDFLARE_DOMAIN="$2"
+    shift
+    ;;
+    *)
+
+    ;;
+esac
+shift
+done
+
+# Create the config directory if it doesn't exist
+if [ ! -d "$HOME/.fd55" ]; then
+    mkdir $HOME/.fd55
+fi
+
+# Make a backup of the existing config file
+if [ -f ~/.fd55/config.ini ]; then
+  echo "INFO: Backing up existing config file to backup-config.ini"
+  cp ~/.fd55/config.ini ~/.fd55/backup-config.ini
+fi
+
+echo "
+##### Running Config tests #####"
+python -m unittest tests/test_config.py || TEST_RESULTS=false
+
+# Check that at least one argument was provided
+if [ -n "$OCI_USER" ] || [ -n "$OCI_FINGERPRINT" ] || [ -n "$OCI_TENANCY" ] || [ -n "$OCI_REGION" ] || [ -n "$OCI_KEY_FILE" ] || [ -n "$OCI_VAULT" ] ||  [ -n "$ARGO_URL" ] || [ -n "$ARGO_TOKEN" ] || [ -n "$CLOUDFLARE_EMAIL" ] || [ -n "$CLOUDFLARE_API_KEY" ] || [ -n "$CLOUDFLARE_DOMAIN" ]; then
+# Create config file
+  cat << EOF > ~/.fd55/config.ini
+[OCI]
+user = $OCI_USER
+fingerprint = $OCI_FINGERPRINT
+tenancy = $OCI_TENANCY
+region = $OCI_REGION
+key_file = $OCI_KEY_FILE
+kms_vault = $OCI_VAULT
+
+[SOPS]
+key_file = key.txt
+
+[ARGOCD]
+url = $ARGO_URL
+api_token = $ARGO_TOKEN
+
+[CLOUDFLARE]
+email = $CLOUDFLARE_EMAIL
+api_key = $CLOUDFLARE_API_KEY
+domain_name = $CLOUDFLARE_DOMAIN
+EOF
+else
+    echo "INFO: Skipping config file creation, no arguments provided."
+    cp ~/.fd55/backup-config.ini ~/.fd55/config.ini
+fi
+
+# TESTS
 echo "
 ##### Running OCI tests #####"
 python -m unittest tests/test_oci.py || TEST_RESULTS=false
@@ -29,14 +138,8 @@ cat << EOF > test-values.yaml
 test:
     key1: value1
 EOF
-if command -v age >/dev/null 2>&1 ; then
-  age-keygen -o key.txt >/dev/null 2>&1
-  python -m unittest tests/test_sops.py || TEST_RESULTS=false
-  rm test-values.yaml enc-values.yaml key.txt 
-else
-  echo "WARN: Age command not found, install Age https://github.com/FiloSottile/age#installation"
-  TEST_RESULTS=false
-fi
+python -m unittest tests/test_sops.py || TEST_RESULTS=false
+
 echo "
 ##### Running ArgoCD tests #####"
 cat << EOF > tmp_argo_app.json
@@ -76,6 +179,14 @@ if [[ "$TEST_RESULTS" = "true" ]] ; then
 else
   echo "### ERROR: Tests failed! ###"
 fi
+
+# Cleanup
+rm test-values.yaml enc-values.yaml key.txt 
+if [ -f ~/.fd55/backup-config.ini ]; then
+  rm ~/.fd55/config.ini
+  cp ~/.fd55/backup-config.ini ~/.fd55/config.ini
+fi
+
 export TEST_RESULTS
 echo "
 ##########################
---
+++ tests/test_config.py
@@ -0,0 +1,43 @@
+import unittest
+from click.testing import CliRunner
+from cli.cli_groups.integration_cli_config import cli_config_argocd, cli_config_cloudflare, cli_config_oci, cli_config_sops
+unittest.TestLoader.sortTestMethodsUsing = None
+
+
+class TestConfig(unittest.TestCase):
+
+    def test1_config_set_argocd(self):
+        """ Test the Config argo set function """
+        runner = CliRunner()
+        result = runner.invoke(
+            cli_config_argocd.set, ['--url', 'https://argo.domain.com', '--api-token', 'Token'])
+        assert result.exit_code == 0
+
+    def test2_config_set_cloudflare(self):
+        """ Test the Config cloudflare set function """
+        runner = CliRunner()
+        result = runner.invoke(
+            cli_config_cloudflare.set, ['--email', 'test@gmail.com', '--api-key', 'Key', '--domain-name', 'domain.com'])
+        assert result.exit_code == 0
+
+    def test3_config_set_oci(self):
+        """ Test the Config oci set function """
+        runner = CliRunner()
+        result = runner.invoke(
+            cli_config_oci.set, ['--user', 'test1', '--fingerprint', 'test2', '--tenancy', 'test3', '--region', 'test4', '--key-file', 'test5'])
+        assert result.exit_code == 0
+
+    def test4_config_set_sops(self):
+        """ Test the Config sops set function """
+        runner = CliRunner()
+        result = runner.invoke(
+            cli_config_sops.set, ['--key-file', 'key.txt'])
+        assert result.exit_code == 0
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestConfig)
+    unittest.TestLoader.sort_test_methods_by_name(
+        suite, sort_key=lambda x: x.lower())
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.main()
