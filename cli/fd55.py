31f2a76f6efd5c7f9626a1dad7739ddd2d0d76b3
davepinhas89@gmail.com <davepinhas89@gmail.com>
2023-01-16 23:42:41 +0000

    Update version to 3.0.2

--- cli/fd55.py
+++ cli/fd55.py
@@ -11,7 +11,7 @@ from cli.cli_groups.integration_cloudflare import cf
 @click.option('-v', '--verbosity', default='info',
               help='Logging level (info, warn, error, debug)',
               required=False)
-@click.version_option(version='3.0.1')
+@click.version_option(version='3.0.2')
 @click.pass_context
 def main(ctx, verbosity):
     import logging
