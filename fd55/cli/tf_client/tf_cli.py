973baabe30eb248a96906d3a5f50a48629cbe755
David Pinhas <davepinhas89@gmail.com>
2023-03-06 01:16:56 +0200

    improve tf functionality

--- fd55/cli/tf_client/tf_cli.py
+++ fd55/cli/tf_client/tf_cli.py
@@ -1,35 +1,55 @@
-from python_terraform import *
+import subprocess
 import logging
 
 logger = logging.getLogger()
 
-
 class TerraformCli:
+    def __init__(self, working_dir):
+        self.working_dir = working_dir
+        self.logger = logging.getLogger(__name__)
 
-    def tf_path(path):
-        tf = Terraform(working_dir=path)
-        return tf
+    def tf_init(self):
+        cmd = ["terraform", "init"]
+        self.logger.info(f"Initializing Terraform in directory {self.working_dir}")
+        try:
+            result = subprocess.run(cmd, cwd=self.working_dir, check=True, capture_output=True, text=True)
+            self.logger.debug(result.stdout)
+            return 0
+        except subprocess.CalledProcessError as e:
+            self.logger.error(e.stderr)
+            return e.returncode
 
-    def tf_init(path):
-        logging.info(
-            TerraformCli.tf_path(path).init(
-                no_color=IsFlagged,
-                refresh=False,
-                capture_output=True)[1])
+    def tf_plan(self, outfile=None):
+        cmd = ["terraform", "plan"]
+        if outfile:
+            cmd.extend(["-out", outfile])
+        self.logger.info(f"Running Terraform plan")
+        try:
+            result = subprocess.run(cmd, cwd=self.working_dir, check=True, capture_output=True, text=True)
+            self.logger.debug(result.stdout)
+            return 0
+        except subprocess.CalledProcessError as e:
+            self.logger.error(e.stderr)
+            return e.returncode
 
-    def tf_plan(path):
-        logging.info(
-            TerraformCli.tf_path(path).plan(
-                no_color=IsFlagged,
-                refresh=False,
-                capture_output=True)[1])
+    def tf_apply(self, planfile):
+        cmd = ["terraform", "apply", planfile]
+        self.logger.info(f"Applying Terraform changes")
+        try:
+            result = subprocess.run(cmd, cwd=self.working_dir, check=True, capture_output=True, text=True)
+            self.logger.debug(result.stdout)
+            return 0
+        except subprocess.CalledProcessError as e:
+            self.logger.error(e.stderr)
+            return e.returncode
 
-    def tf_apply(path):
-        approve = {"auto-approve": True}
-        logging.info(TerraformCli.tf_path(path).apply(**approve)[1])
-
-    def tf_output(path):
-        logging.info(TerraformCli.tf_path(path).output_cmd()[1])
-
-    def tf_destroy(path):
-        logging.info(TerraformCli.tf_path(path).destroy()[1])
+    def tf_destroy(self):
+        cmd = ["terraform", "destroy", "-auto-approve"]
+        self.logger.info(f"Destroying Terraform resources")
+        try:
+            result = subprocess.run(cmd, cwd=self.working_dir, check=True, capture_output=True, text=True)
+            self.logger.debug(result.stdout)
+            return 0
+        except subprocess.CalledProcessError as e:
+            self.logger.error(e.stderr)
+            return e.returncode
