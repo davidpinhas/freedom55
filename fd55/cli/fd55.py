012bdb059b3afcfdecd6ec08f2364bf74515dc2e
davepinhas89@gmail.com <davepinhas89@gmail.com>
2023-03-05 03:07:48 +0000

    Update version to 3.3.0

--- fd55/cli/fd55.py
+++ fd55/cli/fd55.py
@@ -12,7 +12,7 @@ from fd55.cli.cli_groups.integration_ai import ai
 @click.option('-v', '--verbosity', default='info',
               help='Logging level (info, warn, error, debug)',
               required=False)
-@click.version_option(version='3.2.9')
+@click.version_option(version='3.3.0')
 @click.pass_context
 def main(ctx, verbosity):
     import logging
