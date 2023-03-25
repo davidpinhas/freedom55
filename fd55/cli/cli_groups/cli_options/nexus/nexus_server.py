d0b99d9dd9187d4e7cab424399cb43c813b72a48
davepinhas89@gmail.com <davepinhas89@gmail.com>
2023-03-25 15:15:31 +0000

    Update version to 3.5.3

--- fd55/cli/cli_groups/cli_options/nexus/nexus_server.py
+++ fd55/cli/cli_groups/cli_options/nexus/nexus_server.py
@@ -16,11 +16,13 @@ def run_backup_task(ctx):
     """ Run backup task """
     NexusRepositoryManager().run_backup_task()
 
+
 @server.command(help_priority=2)
 @click.pass_context
 def list_repos(ctx):
     """ Retrieve repositories list """
     NexusRepositoryManager().list_repositories()
+
 
 @server.command(help_priority=3)
 @click.pass_context
