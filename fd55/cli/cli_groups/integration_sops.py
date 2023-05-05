ad23c7c4cba4304733f4e09cf8a36e3ef3d62f84
David Pinhas <davepinhas89@gmail.com>
2023-05-05 10:48:22 +0300

    add sops --in-place flag for encrypt/decrypt commands

--- fd55/cli/cli_groups/integration_sops.py
+++ fd55/cli/cli_groups/integration_sops.py
@@ -10,37 +10,40 @@ def sops(ctx):
 
 @sops.command()
 @click.option('-i', '--input-file', help='file to encrypt', required=True)
-@click.option('-o',
-              '--output-file',
-              help='encrypted output file',
-              required=True)
-@click.option('-r',
-              '--encrypted-regex',
-              help='set the encrypted key suffix',
-              required=False)
-@click.option('-k', '--key-file', help='Age full key path', required=False)
+@click.option('-o', '--output-file', help='encrypted output file')
+@click.option('-r', '--encrypted-regex', help='set the encrypted key suffix')
+@click.option('-k', '--key-file', help='Age full key path')
+@click.option('--in-place', help='modify file inplace', is_flag=True)
 @click.pass_context
-def encrypt(ctx, input_file, output_file, encrypted_regex, key_file):
+def encrypt(ctx, input_file, output_file, encrypted_regex, key_file, in_place):
     """ Encrypt file using SOPS with Age encryption """
     from fd55.cli.sops_client.sops_cli import Sops
+    if in_place and output_file:
+        raise click.UsageError("Cannot specify both --in-place and --output-file")
+    if in_place:
+        output_file = input_file
     Sops.encrypt(
         input_file=input_file,
         output_file=output_file,
-        encrypted_regex=encrypted_regex, key_file=key_file)
-
+        encrypted_regex=encrypted_regex,
+        key_file=key_file,
+        in_place=in_place)
 
 @sops.command()
 @click.option('-i', '--input-file', help='file to decrypt', required=True)
-@click.option('-o',
-              '--output-file',
-              help='decrypted output file',
-              required=True)
-@click.option('-k', '--key-file', help='Age full key path', required=False)
+@click.option('-o', '--output-file', help='decrypted output file')
+@click.option('--in-place', help='modify file inplace', is_flag=True)
+@click.option('-k', '--key-file', help='Age full key path')
 @click.pass_context
-def decrypt(ctx, input_file, output_file, key_file=None):
+def decrypt(ctx, input_file, output_file, in_place, key_file=None):
     """ Decrypt file using SOPS with Age encryption """
     from fd55.cli.sops_client.sops_cli import Sops
+    if in_place and output_file:
+        raise click.UsageError("Cannot specify both --in-place and --output-file")
+    if in_place and output_file is None:
+        output_file = input_file
     Sops.decrypt(
         input_file=input_file,
         output_file=output_file,
-        key_file=key_file)
+        key_file=key_file,
+        in_place=in_place)
--- fd55/cli/sops_client/sops_cli.py
+++ fd55/cli/sops_client/sops_cli.py
@@ -49,7 +49,7 @@ class Sops:
             exit()
         return key_id
 
-    def encrypt(input_file, output_file, encrypted_regex=None, key_file=None):
+    def encrypt(input_file, output_file, encrypted_regex=None, key_file=None, in_place=False):
         """ Encrypt file with SOPS using Age """
         logging.info("Encrypting file with SOPS")
         key_id = Sops.verify_key_file(key_file=key_file)
@@ -57,41 +57,61 @@ class Sops:
             key_id = Sops.find_age_key(f"{config.get('SOPS', 'key_file')}")
         logging.debug(f"Using key ID - {key_id}")
         if not encrypted_regex:
-            cmd = ['sops', '--encrypt', '--age', key_id,
-                   '--output', output_file, input_file]
+            if in_place:
+                cmd = ['sops', '--encrypt', '--age', key_id, '--in-place', input_file]
+            else:
+                cmd = ['sops', '--encrypt', '--age', key_id,
+                       '--output', output_file, input_file]
+            logging.debug(f"Running the command - {cmd}")
+            proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if proc.returncode != 0:
+                logging.error(f'Error encrypting file {input_file}: {proc.stderr.decode()}')
+                exit()
+            if in_place:
+                output_file = input_file
         else:
             logging.info(f"Using regex: {encrypted_regex}")
-            cmd = ['sops', '--encrypt', '--age', key_id, '--encrypted-regex',
-                   f'{encrypted_regex}', '--output', output_file, input_file]
-        logging.debug(f"Running the command - {cmd}")
-        proc = subprocess.run(cmd, stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE)
-        if proc.returncode != 0:
-            logging.error(
-                f'Error encrypting file {input_file}: {proc.stderr.decode()}')
-            exit()
+            if in_place:
+                cmd = ['sops', '--encrypt', '--age', key_id, '--encrypted-regex',
+                       f'{encrypted_regex}', '--in-place', input_file]
+            else:
+                cmd = ['sops', '--encrypt', '--age', key_id, '--encrypted-regex',
+                       f'{encrypted_regex}', '--output', output_file, input_file]
+            logging.debug(f"Running the command - {cmd}")
+            proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if proc.returncode != 0:
+                logging.error(f'Error encrypting file {input_file}: {proc.stderr.decode()}')
+                exit()
+            if in_place:
+                output_file = input_file
         logging.info(f"Finished encrypting {output_file} file")
 
-    def decrypt(input_file, output_file, key_file=None):
+    def decrypt(input_file, output_file=None, key_file=None, in_place=False):
         """ Decrypt file with SOPS using Age """
         logging.info("Decrypting file with SOPS")
         key_id = Sops.verify_key_file(key_file=key_file)
         if not key_file:
             key_id = Sops.find_age_key(f"{config.get('SOPS', 'key_file')}")
         logging.debug(f"Using key ID - {key_id}")
+        if in_place:
+            output_file = input_file
+            inplace_arg = "--in-place"
+        else:
+            inplace_arg = ""
+
         cmd = [
             'sops',
             '-d',
             '--age',
             key_id,
-            '--output',
-            output_file,
+            inplace_arg,
             input_file]
+        if output_file:
+            cmd.extend(['--output', output_file])
+
         logging.debug(f"Running the command - {cmd}")
-        proc = subprocess.run(cmd, stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE)
+        proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if proc.returncode != 0:
-            logging.error(
-                f'Error decrypting file "{input_file}" with sops: {proc.stderr.decode()}')
+            logging.error(f'Error decrypting file "{input_file}" with sops: {proc.stderr.decode()}')
             exit()
-        logging.info(f"Finished decrypting {output_file} file")
+        logging.info(f"Finished decrypting {input_file}")
